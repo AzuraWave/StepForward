e71ac62ef51815a73cc56d9cca7c99178a6bb4bb
AzuraWave <AzuraWaves@gmail.com>
2024-08-13 09:08:09 +0300

    Need to fix the update portion.

--- StepForward/Controllers/HomeController.cs
+++ StepForward/Controllers/HomeController.cs
@@ -19,8 +19,11 @@ namespace StepForward.Controllers
         {
             using (var context = new StepForwardContext())
             {
+                 string errormessage = null; 
 
-                var viewModel = _sectionTypeService.GetSectionTypeViewModel();
+                var viewModel = _sectionTypeService.GetSectionTypeViewModel(out errormessage);
+
+
 
                 return View(viewModel);
             }
@@ -29,11 +32,50 @@ namespace StepForward.Controllers
         [HttpPost]
         public ActionResult CreateSectionType(Section_Type sectionType)
         {
-            _sectionTypeService.AddSectionType(sectionType);
 
-            return RedirectToAction("Index");
+            string errormessage = null;
+
+            _sectionTypeService.AddSectionType(sectionType ,out errormessage);
+
+            if (!string.IsNullOrEmpty(errormessage))
+            {
+                ModelState.AddModelError("CreateSectionTypes", errormessage);
+            }
+
+
+
+            var viewModel = _sectionTypeService.GetSectionTypeViewModel(out errormessage);
+            return View("Index", viewModel);
         }
 
+        [HttpPost]
+        public ActionResult DeleteSectionTypes(int[] selectedItems)
+        {
+            string errormessage = null;
+
+            _sectionTypeService.DeleteSectionTypes(selectedItems, out errormessage);
+            if (!string.IsNullOrEmpty(errormessage))
+            {
+                ModelState.AddModelError("DeleteSectionTypes", errormessage);
+            }
+
+
+            var viewModel = _sectionTypeService.GetSectionTypeViewModel(out errormessage);
+            return View("Index", viewModel);
+        }
+
+        [HttpGet]
+        public ActionResult EditSectionType(int id)
+        {
+            
+            
+        }
+
+        [HttpPost]
+        public ActionResult EditSectionType(Section_Type sectionType)
+        {
+            
+        }
 
     }
 }
--- StepForward/Models/SectionTypeModel/SectionTypeService.cs
+++ StepForward/Models/SectionTypeModel/SectionTypeService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc.Html;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json.Serialization;
+using System.Xml.Linq;
 
 
 namespace StepForward.Models
@@ -16,76 +18,115 @@ namespace StepForward.Models
             _context = new StepForwardContext();
         }
 
-        public IEnumerable<Section_Type> GetAllSectionTypes()
+        public IEnumerable<Section_Type> GetAllSectionTypes(out string errormessage)
         {
+            errormessage = null;
             try
             {
+
                 var list = _context.Section_Types.ToList();
 
                 if (list == null || !list.Any())
                 {
-                    throw new Exception(list + " Is Empty");
+                    errormessage = (list + " Is Empty");
                 }
 
                 return list;
             }
             catch (Exception ex)
             {
-                throw;
+                errormessage = ex.Message;
+                return null;
             }
         }
 
-        public SectionTypeViewModel GetSectionTypeViewModel() {
+        public SectionTypeViewModel GetSectionTypeViewModel(out string errormessage1)
+        {
 
+            string errormessage = null;
+            errormessage1 = null;
             try
             {
                 var viewModel = new SectionTypeViewModel
                 {
                     NewSectionType = new Section_Type(),
-                    SectionTypes = GetAllSectionTypes()
+                    SectionTypes = GetAllSectionTypes(out errormessage),
 
                 };
-
+                errormessage1 = errormessage;
                 return viewModel;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                throw;
+                errormessage1 = ex.Message;
+                return null;
             }
         }
 
-        public void AddSectionType(Section_Type sectionType)
+        public void AddSectionType(Section_Type sectionType, out string ADDerrormessage)
         {
-
+            ADDerrormessage = null;
             string name = sectionType.Name;
             try
             {
                 if (string.IsNullOrEmpty(name))
                 {
-                    throw new Exception( "Section Type Name Can't be empty");
+                    ADDerrormessage = "Section Type Name can't be empty.";
+                    return;
                 }
 
-                if (!Regex.IsMatch(name, "^[A-Za-z]+$")) {
-
-                    throw new Exception("Section Type Name must not contain special characters");
+                if (!Regex.IsMatch(name, "^[A-Za-z]+$"))
+                {
+                    ADDerrormessage = "Section Type Name must not contain special characters.";
+                    return;
                 }
 
-                if (name.Length > 10) {
-                    throw new Exception("Section Type Name must be 10 or less characters");
-                } 
+                if (name.Length > 10)
+                {
+                    ADDerrormessage = "Section Type Name must be 10 or fewer characters.";
+                    return;
+                }
 
 
 
                 _context.Section_Types.InsertOnSubmit(sectionType);
                 _context.SubmitChanges();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                throw;
+                ADDerrormessage = ex.Message;
             }
 
 
-            
+
         }
+
+        public void DeleteSectionTypes(int[] selectedItems, out string DELETEerrormessage)
+        {
+            DELETEerrormessage = null;
+            try
+            {
+                if (selectedItems == null || !selectedItems.Any())
+                {
+                    DELETEerrormessage = "No Section Types Selected";
+                    return;
+                }
+
+                var sectionTypes = _context.Section_Types.Where(x => selectedItems.Contains(x.Id)).ToList();
+
+                if (sectionTypes == null || !sectionTypes.Any())
+                {
+                    DELETEerrormessage = "No Section Types Found";
+                    return;
+                }
+                _context.Section_Types.DeleteAllOnSubmit(sectionTypes);
+                _context.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+                DELETEerrormessage = ex.Message;
+            }
+        }
+        
     }
 }
